2269bc4df964ed16bd47c0a4d39a7b18f7e8cf2c
BudrikasMP <84248528+BudrikasMP@users.noreply.github.com>
2021-05-21 12:34:08 +0200

    Update driver.ts

--- driver.ts
+++ driver.ts
@@ -2,7 +2,7 @@
  * PCA9685
  */
 //% weight=100 color=#0fbc11 icon=""
-namespace PCA9685 {
+namespace PCA9685_RU {
     let _DEBUG: boolean = false
     const debug = (msg: string) => {
         if (_DEBUG === true) {
